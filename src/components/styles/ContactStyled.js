0ac17c65abae437b3061ba2209372097f82ed64a
Ezekwu Jerry <ezekwujerry@gmail.com>
2022-11-04 09:22:34 +0100

    Update ContactStyled.js

--- src/components/styles/ContactStyled.js
+++ src/components/styles/ContactStyled.js
@@ -30,7 +30,7 @@ export const StyledContact = styled.main`
         .details{
             padding-right: 6rem;
             a{
-                text-decoration: none;
+                text-decoration: none;  
                 font-size: 1.5rem;
             }
             p{
